d0be2c2171ab39c6a34d498c1672d3df4813a705
arielAlanCruz <acruzcusi@uade.edu.ar>
2026-03-27 16:05:42 -0300

    proyecto actualizado

--- laberinto/src/main/java/com/progra3/laberinto/service/algoritmos/AlgoritmoGeneracion.java
+++
@@ -1,9 +0,0 @@
-package com.progra3.laberinto.service.algoritmos;
-
-import com.progra3.laberinto.model.Celda;
-import java.util.List;
-
-public interface AlgoritmoGeneracion {
-    List<List<Celda>> generarLaberinto(int ancho, int alto, String laberintoId);
-    String getNombre();
-}
